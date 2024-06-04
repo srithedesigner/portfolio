a09230727dacf2862072a03762f6cf50b63ca17a
srithedesigner <srivaishnavsri@gmail.com>
2024-06-04 10:49:04 +0530

    made homepage

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Picture from './components/Picture/Picture';
+import { TypeAnimation } from 'react-type-animation';
+import React, { useEffect, useState } from 'react';
 
 function App() {
+
+  const [name, setName] = useState(['S', 'r', 'i', ' ', 'V', 'a', 'i', 's', 'h', 'n', 'a', 'v'])
+  const targetName = ['శ', '్', 'ర', 'ీ', ' ', 'వ', 'ై', 'ష', '్', 'ణ', 'వ', '్']
+
+  const shuffleName = () => {
+    if (name.join('') == "Sri Vaishnav") {
+      setName(targetName)
+    } else {
+      setName(['S', 'r', 'i', ' ', 'V', 'a', 'i', 's', 'h', 'n', 'a', 'v'])
+    }
+  };
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <title>Sri Vaishnav</title>
+      
+      {/* <h1 id = "title_name">Sri Vaishnav <br/> Vutukuri</h1> */}
+      <TypeAnimation
+      sequence={[
+        'Sri Vaishnav Vutukuri',
+        1000,
+        'శ్రీ వైష్ణవ్ వుటుకూరి',
+        1000,
+      ]}
+      wrapper="span"
+      speed={50}
+      style={{
+        fontSize: '5em', 
+        display: 'inline-block', 
+        color: "white",
+        position: 'absolute',
+        top: '45%',
+        left: '0%',
+        fontFamily: "'Merriweather', serif",
+        fontWeight: 'bold',
+        paddingLeft: "1em"
+      }}
+      repeat={Infinity}
+    />
+      <Picture />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -5,13 +5,11 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+document.title = "Sri Vaishnav"; // Set the title of the website
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
