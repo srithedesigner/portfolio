19b5b8b7e475215c9adfeef871ea0b9dcc568ba3
srithedesigner <srivaishnavsri@gmail.com>
2024-06-13 14:05:36 +0530

    made it somewhat responsive

--- src/App.js
+++ src/App.js
@@ -1,52 +1,36 @@
-
 import './App.css';
 import Picture from './components/Picture/Picture';
 import { TypeAnimation } from 'react-type-animation';
 import React, { useEffect, useState } from 'react';
 
 function App() {
-
-  const [name, setName] = useState(['S', 'r', 'i', ' ', 'V', 'a', 'i', 's', 'h', 'n', 'a', 'v'])
-  const targetName = ['శ', '్', 'ర', 'ీ', ' ', 'వ', 'ై', 'ష', '్', 'ణ', 'వ', '్']
+  const [name, setName] = useState(['S', 'r', 'I', ' ', 'V', 'a', 'I', 's', 'h', 'n', 'a', 'v']);
+  const targetName = ['శ', '్', 'ర', 'ీ', ' ', 'వ', 'ై', 'ష', '్', 'ణ', 'వ', '్'];
 
   const shuffleName = () => {
-    if (name.join('') == "Sri Vaishnav ") {
-      setName(targetName)
+    if (name.join('') === "Sri Vaishnav ") {
+      setName(targetName);
     } else {
-      setName(['S', 'r', 'i', ' ', 'V', 'a', 'i', 's', 'h', 'n', 'a', 'v'])
+      setName(['S', 'r', 'i', ' ', 'V', 'a', 'i', 's', 'h', 'n', 'a', 'v']);
     }
   };
 
-
-
-
   return (
-    <div>
+    <div className="container">
       <title>Sri Vaishnav</title>
       
-      {/* <h1 id = "title_name">Sri Vaishnav <br/> Vutukuri</h1> */}
       <TypeAnimation
-      sequence={[
-        'Sri Vaishnav Vutukuri',
-        1000,
-        'శ్రీ వైష్ణవ్ వుటుకూరి',
-        1000,
-      ]}
-      wrapper="span"
-      speed={50}
-      style={{
-        fontSize: '5em', 
-        display: 'inline-block', 
-        color: "white",
-        position: 'absolute',
-        top: '45%',
-        left: '0%',
-        fontFamily: "'Merriweather', serif",
-        fontWeight: 'bold',
-        paddingLeft: "1em"
-      }}
-      repeat={Infinity}
-    />
+        sequence={[
+          'Sri Vaishnav Vutukuri',
+          1000,
+          'శ్రీ వైష్ణవ్ వుటుకూరి',
+          1000,
+        ]}
+        wrapper="span"
+        speed={50}
+        className="name-animation"
+        repeat={Infinity}
+      />
       <Picture />
     </div>
   );
